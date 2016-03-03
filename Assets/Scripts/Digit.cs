5b53eb40f88b2fd83ec8e6839bf9d634f150b732
Christopher Steven Hamilton <cshamilton@berkeley.edu>
2016-03-02 18:29:28 -0800

    Merged new sprites

---
+++ Assets/Scripts/Digit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Digit : MonoBehaviour {
+
+    public Sprite[] digits;    
+    private SpriteRenderer sr;
+
+	public void SpriteUpdate(int num) {
+        GetComponent<SpriteRenderer>().sprite = digits[num];
+    }
+}
