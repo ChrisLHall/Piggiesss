878fcaf36faf01a6fe2b40fae84b5efbe956b10f
ChrisLHall <mrchrislhall@gmail.com>
2016-03-06 11:55:29 -0800

    Poop tracker and UI stick to the screen

--- Assets/Scripts/PoopTracker.cs
+++ Assets/Scripts/PoopTracker.cs
@@ -1,47 +1,56 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System;
 
 public class PoopTracker : MonoBehaviour {
+
+    const int MAX = 999999;
 
     public long amount;
     public GameObject digitsPrefab;
     private GameObject[] oldDigits;
 
     void Start() {
-        oldDigits = new GameObject[10];
+        oldDigits = new GameObject[MAX.ToString().Length];
+        InitSprites();
         updateSprite();
     }
 
+    void InitSprites () {
+        for (int i = 0; i < oldDigits.Length; i++) {
+            oldDigits[i] = Instantiate<GameObject>(digitsPrefab);
+            oldDigits[i].transform.SetParent(transform);
+        }
+    }
 
     // Called on every update of poop count to update sprite.
     private void updateSprite() {
 
-        // Remove old game digits.
-        foreach (GameObject g in oldDigits) {
-            Destroy(g);
-        }
-
-        Vector3 newPosition = new Vector3(2.15f, .75f, 0f);
+        Vector3 newPosition = new Vector3();
 
         // Maximum amount of poop allowed.
-        amount = Math.Min(amount, 999999);
+        amount = Math.Min(amount, MAX);
         string str_num = amount.ToString();
 
         // Create new objects based on the new string.
         for (int i = 0; i < str_num.Length; i++) {
 
             // Create and add to list.
-            GameObject newDigit = Instantiate<GameObject>(digitsPrefab);
-            oldDigits[i] = newDigit;
+            GameObject newDigit = oldDigits[i];
+            newDigit.SetActive(true);
 
             // Set position.
-            newDigit.transform.position = newPosition;
+            newDigit.transform.localPosition = newPosition;
             newPosition.x += .08f;
 
             // Set sprite's number value.
             Digit script = newDigit.GetComponent<Digit>();
             script.SpriteUpdate((int)Char.GetNumericValue(str_num[i]));
+        }
+
+        for (int i = str_num.Length; i < oldDigits.Length; i++) {
+            GameObject newDigit = oldDigits[i];
+            newDigit.SetActive(false);
         }
     }
 
