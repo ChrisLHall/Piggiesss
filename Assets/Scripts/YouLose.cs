18f0543be23a24591fa35a9f308bee97c15f8199
ChrisLHall <mrchrislhall@gmail.com>
2016-05-04 11:34:30 -0700

    High scores are printed.

--- Assets/Scripts/YouLose.cs
+++ Assets/Scripts/YouLose.cs
@@ -40,64 +40,67 @@ public class YouLose : MonoBehaviour {
     IEnumerator LoseCheck () {
         for (;;) {
             yield return new WaitForSeconds(0.1f);
-            if (tl.Done || endGame) {
+            if ((tl.Done || endGame) && !lost) {
 
-                foreach (Poop poo in FindObjectsOfType<Poop>()) {
-                    Destroy(poo.gameObject);
-                }
+                //foreach (Poop poo in FindObjectsOfType<Poop>()) {
+                //    Destroy(poo.gameObject);
+                //}
                 Toolbar t = FindObjectOfType<Toolbar>();
                 t.poopCounter.amount = 0;
+                FindObjectOfType<Farmer>().GetComponent<Collider2D>().enabled = false;
                 foreach (Transform child in transform) {
                     child.gameObject.SetActive(true);
                 }
-                text.text = "You Lose! Final score: " + t.scoreCounter.amount + "\n\nRestart";
+                text.text = "Time up!\nFinal score: " + t.scoreCounter.amount + "\nTap to restart";
                 lossTime = Time.time;
                 lost = true;
 
                 int current = score.amount;
+                List<string> scores = null;
 
                 // If the scores were already saved.
                 if (!prevSavedScore) {
 
                     string savedScores = "";
 
-                    // If scores, was already initialized.
-                    if (PlayerPrefs.HasKey("scores") && !resetScores) {
-
-                        // Load the current high scores and compare with the current.
-                        List<string> scores = new List<string>(PlayerPrefs.GetString("scores").Split(','));
-                        for (int i = 0; i < 5; i ++) {
-                            if (current > Int32.Parse(scores[i])) {
-                                scores.Insert(i, current.ToString());
-                                break;
-                            }
-                        }
-
-                        // Save the newly found high scores to a string and save.
-                        for (int i = 0; i < 5; i ++) {
-                            savedScores += scores[i] + ",";
-                        }
-
-                        PlayerPrefs.SetString("scores", savedScores.Remove(savedScores.Length - 1));
-                        PlayerPrefs.Save();
-
-                    // Initializing the high scores.
-                    } else {
-                        savedScores = ",0,0,0,0";
+                    if (!PlayerPrefs.HasKey("scores") || resetScores) {
+                        savedScores = "0,0,0,0,0";
                         PlayerPrefs.SetString("scores", current.ToString() + savedScores);
                         PlayerPrefs.Save();
                         resetScores = false;
                     }
 
+                    // Load the current high scores and compare with the current.
+                    scores = new List<string>(PlayerPrefs.GetString("scores").Split(','));
+                    for (int i = 0; i < 5; i ++) {
+                        if (current > Int32.Parse(scores[i])) {
+                            scores.Insert(i, current.ToString());
+                            break;
+                        }
+                    }
+
+                    while (scores.Count > 5) {
+                        scores.RemoveAt(scores.Count - 1);
+                    }
+                    // Save the newly found high scores to a string and save.
+                    savedScores += string.Join(",", scores.ToArray());
+
+                    PlayerPrefs.SetString("scores", savedScores);
+                    PlayerPrefs.Save();
+
                     prevSavedScore = true;
                     Debug.Log("Current savedScores: " + savedScores);
+
                 }
 
-        
-            }
 
-            int pigs = FindObjectsOfType<Pig>().Length;
-            
+                text.text += "\n\nHigh scores:\n";
+                for (int i = 0; i < 5; i++) {
+                    if (Int32.Parse(scores[i]) > 0) {
+                        text.text += scores[i] + "\n";
+                    }
+                }
+            }
         }
     }
 
